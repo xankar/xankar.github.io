b63a2e4c90ec952938afb519d7791a97b6b53b52
xankar <xanketh@gmail.com>
2018-03-24 22:20:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $.get("file:///C:/Users/xanke/Desktop/html%20pages/navi.html", function(data) {
+  $.get("navi.html", function(data) {
     $("#header").html(data);
   });
 });
